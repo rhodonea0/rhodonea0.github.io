48ebb1d046fcf02806c30aefde6d283df2f81e6c
Mikey Higgs <83879322+rhodonea0@users.noreply.github.com>
2021-05-08 18:26:16 +0100

    Test

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  window.onscroll = function() {myFunction()};
+
+  let navbar = document.getElementById("navbar");
+
+  let sticky = navbar.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")} 
+    
+    else {
+      navbar.classList.remove("sticky");}}
+      
+})
